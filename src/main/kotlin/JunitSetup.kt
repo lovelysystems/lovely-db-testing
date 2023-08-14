7fd9b696552a35bf1aa9f23af5baf393497baa12
dobe <bernddorn@gmail.com>
2023-08-14 08:54:12 +0200

    allow to reuse the server container

--- src/main/kotlin/JunitSetup.kt
+++ src/main/kotlin/JunitSetup.kt
@@ -6,6 +6,21 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 import kotlin.io.path.Path
 
+/**
+ * Settings for a PGTestSetup
+ *
+ * @param clientImage docker image for the client container
+ * @param serverImage docker image for the server container
+ * @param devDir directory with sql files to be mapped into the server container
+ * @param testDir directory with sql files to be tested
+ * @param defaultDB default database to connect to
+ * @param testFilePattern glob pattern for sql files to be tested
+ * @param resetScripts list of sql files to be executed before each reset
+ * @param reuseIdent enables reuse of the server container. If set to null, a new container is started for each test.
+ * @param fixedPort if set, the server's postgres port will be exposed on this host port. If null, a random port is used.
+ * @param serverConfiguration configuration block for the server container
+ * @param clientConfiguration configuration block for the client container
+ */
 data class PGTestSettings(
     val clientImage: String = "lovelysystems/docker-postgres:14.1.0-client",
     val serverImage: String = "lovelysystems/docker-postgres:14.1.0",
@@ -14,6 +29,8 @@ data class PGTestSettings(
     val defaultDB: String = "postgres",
     val testFilePattern: String = "**.sql",
     val resetScripts: List<String> = emptyList(),
+    val reuseIdent: String? = null,
+    val fixedPort: Int? = null,
     val serverConfiguration: PGServerContainer.() -> Unit = {},
     val clientConfiguration: PGClientContainer.() -> Unit = {},
 ) {
@@ -28,7 +45,9 @@ data class PGTestSettings(
                 configureBlock = clientConfiguration
             ),
             PGServerContainer(serverImage, configureBlock = serverConfiguration),
-            resetScripts = resetScripts
+            resetScripts = resetScripts,
+            reuseIdent = reuseIdent,
+            fixedPort = fixedPort
         )
     }
 }
--- src/main/kotlin/PGTestSetup.kt
+++ src/main/kotlin/PGTestSetup.kt
@@ -5,7 +5,9 @@ import org.testcontainers.containers.Network
 class PGTestSetup(
     val client: PGClientContainer,
     val server: PGServerContainer,
-    private val resetScripts: List<String>
+    private val resetScripts: List<String>,
+    private val reuseIdent: String? = null,
+    private val fixedPort: Int? = null,
 ) {
 
     fun reset() {
@@ -15,15 +17,33 @@ class PGTestSetup(
     }
 
     fun start() {
-        val network = Network.newNetwork()
+        val network = if (reuseIdent != null) {
+            // use a network named after the site
+            createOrUseNetwork("ldbt_$reuseIdent")
+        } else {
+            Network.newNetwork()
+        }
+        if (reuseIdent == null) {
+            client.withNetworkAliases("client")
+            server.withNetworkAliases("server")
+        }
+
         client.withNetwork(network)
         server.withNetwork(network)
+        server.withReuse(reuseIdent != null)
+        fixedPort?.let {
+            server.withFixedPort(it)
+        }
+
+
         server.start()
         client.start()
     }
 
     fun stop() {
-        client.stop()
-        server.stop()
+        if (reuseIdent == null) {
+            client.stop()
+            server.stop()
+        }
     }
 }
